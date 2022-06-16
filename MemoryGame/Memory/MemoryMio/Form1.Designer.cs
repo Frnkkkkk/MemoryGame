272f328f317fb5bfc2dae40c3b773a0d722ed0ae
Frnkkkkk <107641679+Frnkkkkk@users.noreply.github.com>
2022-06-16 19:25:07 +0200

    Add files via upload

---
+++ MemoryGame/Memory/MemoryMio/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace MemoryMio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.TimerSecondario = new System.Windows.Forms.Timer(this.components);
+            this.labelMosse = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RadioEasy = new System.Windows.Forms.RadioButton();
+            this.RadioHard = new System.Windows.Forms.RadioButton();
+            this.LabelDifficulty = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TimerSecondario
+            // 
+            this.TimerSecondario.Tick += new System.EventHandler(this.TimerSecondario_Tick);
+            // 
+            // labelMosse
+            // 
+            this.labelMosse.AutoSize = true;
+            this.labelMosse.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMosse.Location = new System.Drawing.Point(10, 10);
+            this.labelMosse.Name = "labelMosse";
+            this.labelMosse.Size = new System.Drawing.Size(136, 36);
+            this.labelMosse.TabIndex = 0;
+            this.labelMosse.Text = "Mosse: 0";
+            this.labelMosse.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(63, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Gioca";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RadioEasy
+            // 
+            this.RadioEasy.AutoSize = true;
+            this.RadioEasy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadioEasy.Location = new System.Drawing.Point(63, 78);
+            this.RadioEasy.Name = "RadioEasy";
+            this.RadioEasy.Size = new System.Drawing.Size(89, 29);
+            this.RadioEasy.TabIndex = 2;
+            this.RadioEasy.TabStop = true;
+            this.RadioEasy.Text = "Facile";
+            this.RadioEasy.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.RadioEasy.UseVisualStyleBackColor = true;
+            this.RadioEasy.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // RadioHard
+            // 
+            this.RadioHard.AutoSize = true;
+            this.RadioHard.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadioHard.Location = new System.Drawing.Point(63, 113);
+            this.RadioHard.Name = "RadioHard";
+            this.RadioHard.Size = new System.Drawing.Size(98, 29);
+            this.RadioHard.TabIndex = 3;
+            this.RadioHard.TabStop = true;
+            this.RadioHard.Text = "Difficile";
+            this.RadioHard.UseVisualStyleBackColor = true;
+            this.RadioHard.CheckedChanged += new System.EventHandler(this.RadioHard_CheckedChanged);
+            // 
+            // LabelDifficulty
+            // 
+            this.LabelDifficulty.AutoSize = true;
+            this.LabelDifficulty.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelDifficulty.Location = new System.Drawing.Point(61, 45);
+            this.LabelDifficulty.Name = "LabelDifficulty";
+            this.LabelDifficulty.Size = new System.Drawing.Size(112, 30);
+            this.LabelDifficulty.TabIndex = 5;
+            this.LabelDifficulty.Text = "Difficoltà";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 239);
+            this.Controls.Add(this.LabelDifficulty);
+            this.Controls.Add(this.RadioHard);
+            this.Controls.Add(this.RadioEasy);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelMosse);
+            this.Name = "Form1";
+            this.Text = "Memory";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer TimerSecondario;
+        private System.Windows.Forms.Label labelMosse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton RadioEasy;
+        private System.Windows.Forms.RadioButton RadioHard;
+        private System.Windows.Forms.Label LabelDifficulty;
+    }
+}
+
---
+++ MemoryGame/Memory/MemoryMio/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Media;
+
+namespace MemoryMio
+{
+    public partial class Form1 : Form
+    {
+        string name = System.IO.Directory.GetCurrentDirectory() + "/../../../lil/";
+        string back = System.IO.Directory.GetCurrentDirectory() + "/../../../lil/dorso1.jpg";
+        SoundPlayer WinSound = new SoundPlayer(System.IO.Directory.GetCurrentDirectory() + "/../../../lil/WinningSound.wav");
+        string[] appoggio = new string[50];
+        int[] level = {1,4,6};
+        bool click1 = false;
+        PictureBox[] Pic = new PictureBox[50];
+        Random Numero = new Random();
+        int Valore, i, I, j, k=1, primoCliccato=-1, secondoCliccato=-1, Punteggio = 0, Mosse = 0,difficulty;
+        bool Click1 = false, Click2 = false;
+
+        private void RadioHard_CheckedChanged(object sender, EventArgs e)
+        {
+            difficulty = 2;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            difficulty = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            labelMosse.Location = new System.Drawing.Point(10, 10);
+            button1.Visible = false;
+            labelMosse.Visible = true;
+            RadioEasy.Visible = false;
+            RadioHard.Visible = false;
+            LabelDifficulty.Visible = false;
+            if (difficulty == 1)
+            {
+                Width = 600;
+                Height = 500;
+            }
+            else if (difficulty == 2)
+            {
+                Width = 775;
+                Height = 700;
+            }
+            for (I = 0; I < level[difficulty] * level[difficulty]; I++)
+                appoggio[I] = "";
+
+            for (I = 0; I < level[difficulty] * level[difficulty] / 2; I++)
+            {
+                Valore = Numero.Next(0, level[difficulty] * level[difficulty]);
+                while (appoggio[Valore] != "")
+                    Valore = Numero.Next(0, level[difficulty] * level[difficulty]);
+                appoggio[Valore] = name + Convert.ToString(I) + ".jpg";
+                Valore = Numero.Next(0, level[difficulty] * level[difficulty]);
+                while (appoggio[Valore] != "")
+                    Valore = Numero.Next(0, level[difficulty] * level[difficulty]);
+                appoggio[Valore] = name + Convert.ToString(I) + ".jpg";
+
+            }
+
+            for (i = 0; i < level[difficulty] * level[difficulty]; ++i)
+            {
+                Pic[i] = new PictureBox();
+                Pic[i].Load(appoggio[i]);
+                Pic[i].Enabled = true;
+                Pic[i].Height = 100;
+                Pic[i].Width = 100;
+                Pic[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                Pic[i].Location = new System.Drawing.Point(
+                    10 + k * 105, 10 + j * 105);
+                k++;
+                this.Controls.Add(Pic[i]);
+                if (i == level[difficulty]-1 || i == 2* level[difficulty] - 1 || i == 3* level[difficulty] - 1
+                    || i==4* level[difficulty] - 1 || i==5* level[difficulty] - 1 || i== 6*level[difficulty] - 1)
+                {
+                    j++;
+                    k = 1;
+                }
+
+                Pic[i].Click += new EventHandler(this.ClickSuImmagine);
+                Pic[i].Tag = i;
+            }
+
+            timer1.Interval = 2000;
+            timer1.Tick += (timer1_Tick);
+            timer1.Start();
+        }
+
+        private void TimerSecondario_Tick(object sender, EventArgs e)
+        {
+            TimerSecondario.Stop();
+            if (Click1)
+            {
+                if (appoggio[primoCliccato] == appoggio[secondoCliccato])
+                {
+                    Pic[primoCliccato].Enabled = false;
+                    Pic[secondoCliccato].Enabled = false;
+                    Punteggio++;
+                }
+                else
+                {
+                    Pic[primoCliccato].Load(back);
+                    Pic[secondoCliccato].Load(back);
+                    Pic[primoCliccato].Enabled = true;
+                    Pic[secondoCliccato].Enabled = true;
+                }
+                AzzeraClick(sender, e);
+                Mosse++;
+                labelMosse.Text = "Mosse: " + Convert.ToString(Mosse);
+                if (Punteggio == level[difficulty]*level[difficulty]/2)
+                {
+                    WinSound.Play();
+                    
+                }
+                
+            }
+            Click1 = false;
+           
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        public void AzzeraClick(object sender, EventArgs e)
+        {
+            primoCliccato = -1;
+            secondoCliccato = -1;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (i = 0; i < level[difficulty]*level[difficulty]; i++)
+            {
+                Pic[i].Load(back);
+            }
+            timer1.Stop();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        public void ClickSuImmagine(object sender, EventArgs e)
+        {
+            PictureBox PicCliccata = (PictureBox)sender;
+            PicCliccata.Load(appoggio[(int)PicCliccata.Tag]);
+            
+            if (primoCliccato < 0)
+            {
+                primoCliccato = (int)PicCliccata.Tag;
+                PicCliccata.Enabled = false;
+            }
+            if ((primoCliccato>=0)&&(secondoCliccato<0)&&(Click1==true))
+            {
+                secondoCliccato = (int)PicCliccata.Tag;
+                TimerSecondario.Interval=500;
+                TimerSecondario.Tick += (TimerSecondario_Tick);
+                TimerSecondario.Start();
+            }
+            else
+            {
+                Click1 = true;
+            }
+        }
+    }
+}
